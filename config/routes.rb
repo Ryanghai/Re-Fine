7680ee8882bef5f6e6fa048efcbdcf6cbcc8ae7a
GitHubにRyanghai <68055743+Ryanghai@users.noreply.github.com>
2020-08-21 14:11:10 +0900

    modify...

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+
+  # TOP
+  get 'pages/index'
+
+  # Exhibitoinsの一覧と詳細ページ
   get '/exhibitions', to: 'exhibitions#index'
   get '/exhibitions/:id', to: 'exhibitions#show'
 
-  resources :users
+  # User
+  # resources :users
+  get '/users', to: 'users#index'
+  get '/users/new', to: 'users#new'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show'
+  get '/users/:id/edit', to: 'users#edit'
+  put '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
 end
